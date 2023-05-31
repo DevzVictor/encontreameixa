4b3d51e03f26476bdc438d3e41004169c7efdf7f
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-05-31 10:24:52 -0300

    feat: fix styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,16 +5,21 @@ import "./Style.css";
 function Footer() {
   return (
     <footer className="footer-container">
-      <div className="footer-text">
-        <h6>Politica de privacidade | Copyright © 2023</h6>
-      </div>
-      <div className="social-icons">
-        <a href="https://www.instagram.com/ameixagoiana/" target="blank">
-          <img src={instaIcon} alt="Instagram Icon" />
-        </a>
-        <a href="https://www.linkedin.com/company/ameixagoiana/" target="blank">
-          <img src={linkedinIcon} alt="Linkedin Icon" />
-        </a>
+      <div className="footer-box">
+        <div className="footer-text">
+          <h6>Politica de privacidade | Copyright © 2023</h6>
+        </div>
+        <div className="social-icons">
+          <a href="https://www.instagram.com/ameixagoiana/" target="blank">
+            <img src={instaIcon} alt="Instagram Icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/ameixagoiana/"
+            target="blank"
+          >
+            <img src={linkedinIcon} alt="Linkedin Icon" />
+          </a>
+        </div>
       </div>
     </footer>
   );
